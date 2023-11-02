12e6f488c955d06783f72deffcb6b1d6c26ae6ff
João Victor Vasconcelos Flôr <danteopzz@hotmail.com>
2023-11-02 14:49:13 -0300

    Updated

--- src/pages/paymentPage/index.tsx
+++ src/pages/paymentPage/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 
 import { MdPix } from 'react-icons/md';
 import { FaCopy, FaCreditCard } from 'react-icons/fa';
-import { useState, useContext } from 'react';
+import { useState, useContext, FormEvent } from 'react';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { CartContext } from '../../services/hooks/useCart/useCart';
@@ -13,12 +13,19 @@ export default function PaymentPage() {
 
     const [cardPayment, setCardPayment] = useState(false)
     const [pixPayment, setPixPayment] = useState(false)
+    const [processToPayment, setProcessToPayment] = useState(false);
 
     function handleOpenCardPayment() {
         setCardPayment(!cardPayment);
     }
     function handleOpenPixPayment() {
         setPixPayment(!pixPayment);
+    }
+
+    function handleOpenProcessToPayment(e?:FormEvent) {
+        e?.preventDefault();
+
+        setProcessToPayment(!processToPayment)
     }
 
     return (
@@ -28,15 +35,42 @@ export default function PaymentPage() {
             </Head>
 
             <div className={`row m-auto container py-5 ${styles.paymentContainer}`}>
-                <div className={`col-md-6 ${styles.formContainer}`}>
-                    <div className={`d-flex gap-3 ${styles.paymentMethodBox}`}>
-                        <button onClick={handleOpenPixPayment} className='d-flex justify-content-center align-items-center gap-3'>
-                            <MdPix className={styles.pix} /> PIX
-                        </button>
-                        <button onClick={handleOpenCardPayment} className='d-flex justify-content-center align-items-center gap-3'>
-                            <FaCreditCard className={styles.cardIcon} /> Cartão
-                        </button>
-                    </div>
+                <div className={`col-md-6 mb-5 ${styles.formContainer}`}>
+                    <form className='d-flex flex-column gap-3'>
+                        <div className={` ${styles.inputBox}`}>
+                            <label className='mb-3 fw-bold' htmlFor="address">Endereço</label>
+                            <Input required id='address' type='text' placeholder='Rua Sebastião Porto' />
+                        </div>
+                        <div className={` ${styles.inputBox}`}>
+                            <label className='mb-3 fw-bold' htmlFor="city">Cidade</label>
+                            <Input required id='city' type='text' placeholder='Santo Lorem' />
+                        </div>
+                        <div className={`d-flex gap-5 ${styles.inputBox}`}>
+                            <div>
+                                <label className='mb-3 fw-bold' htmlFor="state">Estado</label>
+                                <Input required id='state' type='text' placeholder='RJ' />
+                            </div>
+                            <div>
+                                <label className='mb-3 fw-bold' htmlFor="cep">CEP</label>
+                                <Input required id='cep' type='number' placeholder='XXXXXXXX' />
+                            </div>
+                        </div>
+                        <div className='m-auto'>
+                            <Button onClick={handleOpenProcessToPayment}>Prosseguir</Button>
+                        </div>
+                    </form>
+
+                    {processToPayment && (
+                        <div className={`d-flex gap-3 mt-5 ${styles.paymentMethodBox}`}>
+                            <button onClick={handleOpenPixPayment} className='d-flex justify-content-center align-items-center gap-3'>
+                                <MdPix className={styles.pix} /> PIX
+                            </button>
+                            <button onClick={handleOpenCardPayment} className='d-flex justify-content-center align-items-center gap-3'>
+                                <FaCreditCard className={styles.cardIcon} /> Cartão
+                            </button>
+                        </div>
+                    )}
+
                     {pixPayment && (
                         <div className={`mt-5 ${styles.pixMethod}`}>
                         <h5 className='fw-bold'>Escaneie o QR Code em sua tela</h5>
@@ -55,27 +89,27 @@ export default function PaymentPage() {
 
                     {cardPayment && (
                         <form className='d-flex flex-column gap-3 mt-5'>
-                        <div className={` ${styles.inputBox}`}>
-                            <label className='mb-3 fw-bold' htmlFor="cardName">Nome Impresso no Cartão</label>
-                            <Input id='cardName' type='text' placeholder='JOHN DOE' />
-                        </div>
-                        <div className={` ${styles.inputBox}`}>
-                            <label className='mb-3 fw-bold' htmlFor="cardNumber">Número do Cartão</label>
-                            <Input id='cardNumber' type='number' placeholder='XXXX-XXXX-XXXX' />
-                        </div>
-                        <div className={`d-flex gap-5 ${styles.inputBox}`}>
-                            <div>
-                                <label className='mb-3 fw-bold' htmlFor="dateCard">Validade do Cartão</label>
-                                <Input id='dateCard' type='number' placeholder='04/2030' />
+                            <div className={` ${styles.inputBox}`}>
+                                <label className='mb-3 fw-bold' htmlFor="cardName">Nome Impresso no Cartão</label>
+                                <Input id='cardName' type='text' placeholder='JOHN DOE' />
                             </div>
-                            <div>
-                                <label className='mb-3 fw-bold' htmlFor="cvc">CVC</label>
-                                <Input id='cvc' type='number' placeholder='123' />
+                            <div className={` ${styles.inputBox}`}>
+                                <label className='mb-3 fw-bold' htmlFor="cardNumber">Número do Cartão</label>
+                                <Input id='cardNumber' type='number' placeholder='XXXX-XXXX-XXXX' />
                             </div>
-                        </div>
-                        <div className='m-auto'>
-                            <Button>Realizar Pagamento</Button>
-                        </div>
+                            <div className={`d-flex gap-5 ${styles.inputBox}`}>
+                                <div>
+                                    <label className='mb-3 fw-bold' htmlFor="dateCard">Validade do Cartão</label>
+                                    <Input id='dateCard' type='number' placeholder='04/2030' />
+                                </div>
+                                <div>
+                                    <label className='mb-3 fw-bold' htmlFor="cvc">CVC</label>
+                                    <Input id='cvc' type='number' placeholder='123' />
+                                </div>
+                            </div>
+                            <div className='m-auto'>
+                                <Button>Realizar Pagamento</Button>
+                            </div>
                         </form>
                     )}
                 </div>
